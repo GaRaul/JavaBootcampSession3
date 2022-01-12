2d2bf9f61b224e8b5f3cf0ab3a1d7bd62972636e
Raúl <rgarridorasillo@gmail.com>
2022-01-12 11:45:49 +0100

    TAE5 1

---
+++ src/BuclesApp.java
@@ -0,0 +1,19 @@
+
+public class BuclesApp {
+
+	public static void main(String[] args) {
+
+		int num1 = 10;
+		int num2 = 10;
+		
+		if (num1 > num2) {
+			System.out.println("Num1: " + num1 + " es mayor que Num2: " + num2);
+		} else if (num1 == num2) {
+			System.out.println("Num1: " + num1 + " es igual a Num2: " + num2);
+		} else {
+			System.out.println("Num2: " + num2 + " es mayor que Num1: " + num1);
+		}
+		
+	}
+
+}
